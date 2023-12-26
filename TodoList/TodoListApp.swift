68576deb5a977418b2403e39879650f386686833
medhatmebed <medhat.mebed@gmail.com>
2023-12-26 07:49:48 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Medhat Mebed on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
